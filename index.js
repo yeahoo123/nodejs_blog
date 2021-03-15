a490db0cb68286f4c2fb4f6e8e8d11160277984a
Son <lamkimson1998@gmail.com>
2021-03-15 12:44:05 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const express = require("express")
+const app = express()
+const port = 4000
+
+app.get('/', (req, res)=>res.send('hello world'))
+
+app.listen(port, ()=>console.log(`Example app listen at http://localhost:${port}`))
